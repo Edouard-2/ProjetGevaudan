5e5f2bbff15480955c5329d92536ef6719989f9c
Edouard-2 <73876708+Edouard-2@users.noreply.github.com>
2021-11-21 16:10:37 +0100

    Finish V1.2 Zoom DeZoom

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -5,21 +5,34 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     Vector3 initPosition;
+    Vector3 NextPosition;
+
+    public float cameraSpeed = 0.1f;
 
     private void Start()
     {
         initPosition = gameObject.transform.position;
+        NextPosition = Vector3.zero;
     }
+
+    private void Update()
+    {
+        if(NextPosition != Vector3.zero && gameObject.transform.position != NextPosition)
+        {
+            Debug.Log("zefojn");
+            gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, NextPosition, cameraSpeed);
+        }
+    }
+
     public void moveCamera(Transform _hit, bool _switch)
     {
         if (_switch)
         {
-            gameObject.transform.position = _hit.GetComponent<ScrollingManager>().cameraPosition;
+            NextPosition = _hit.GetComponent<ScrollingManager>().cameraPosition;
         }
         else
         {
-            gameObject.transform.position = initPosition;
+            NextPosition = initPosition;
         }
-
     }
 }
